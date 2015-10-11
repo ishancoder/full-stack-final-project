cb90a17dcc6f374212ece85f1dd8f6e568d500ba
ishancoder <isaxena171@gmail.com>
2015-10-11 19:49:59 +0530

    created template with some html and added database_setup.py and static

---
+++ database_setup.py
@@ -0,0 +1,14 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base;
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+########################################################
+
+#setup your database here!!!!
+
+############INSERT AT THE END OF THE FILE#####################################
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
