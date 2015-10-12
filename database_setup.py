c9e1792259289d53701d519e4d9d515a55a06213
ishancoder <isaxena171@gmail.com>
2015-10-12 19:14:53 +0530

    added shelter's code in database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,37 @@ Base = declarative_base()
 ########################################################
 
 #setup your database here!!!!
+class Shelter(Base):
+	__tablename__ = 'shelter'
+	id  = Column(
+			Integer,
+			primary_key = True
+		)
+
+	name = Column(
+			String(80),
+			nullable = False
+		)
+
+	address = Column(
+			String(400),
+			nullable = False
+		)
+
+	phone = Column(
+			String(15),
+			nullable = False
+		)
+
+	email = Column(
+			String(50),
+			nullable = False
+		)
+
+	owner = Column(
+			String(80),
+			nullable = False
+		)
 
 ############INSERT AT THE END OF THE FILE#####################################
 engine = create_engine('sqlite:///restaurantmenu.db')
