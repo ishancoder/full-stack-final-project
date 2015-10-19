60349a5d78a8a85a1c18fde03c85d43c89806eb8
ishancoder <isaxena171@gmail.com>
2015-10-19 20:01:00 +0530

    added some comments for help

--- project.py
+++ project.py
@@ -12,12 +12,14 @@ session = DBSession()
 app = Flask(__name__)
 
 
+#the main page routing
 @app.route("/")
 @app.route("/shelters/")
 def allShelters():
 	shelters = session.query(Shelter).all()
 	return render_template("shelters.html", shelters = shelters)
 
+#routing for new shelter creation
 @app.route("/shelters/new/", methods = ['GET','POST'])
 def createShelter():
 	if request.method == 'POST':
@@ -31,6 +33,7 @@ def createShelter():
 		return redirect(url_for('allShelters'))
 	return render_template('addshelter.html')
 
+#routing for updation of a shelter
 @app.route("/shelters/<int:shelter_id>/update/", methods=['GET','POST'])
 def updateShelter(shelter_id):
 	shelter = session.query(Shelter).filter_by(id = shelter_id).one()
@@ -45,22 +48,26 @@ def updateShelter(shelter_id):
 		return redirect(url_for('allShelters'))
 	return render_template('updateshelter.html',shelter = shelter)
 
+#routing for deletion of shelter
 @app.route("/shelters/<int:shelter_id>/delete/")
 def deleteShelter(shelter_id):
 	return "<h1>Delete puppy shelter</h1>"	
 
+#routing for all puppies page
 @app.route("/shelters/<int:shelter_id>/puppies/")
 def allPuppies(shelter_id):
 	puppies = session.query(Puppy).filter_by(shelter_id = shelter_id).all()
 	shelter = session.query(Shelter).filter_by(id = shelter_id).one()
 	return render_template('puppies.html', shelter = shelter, puppies = puppies)
 
+#routing for a specific puppy profile
 @app.route("/shelters/<int:shelter_id>/puppies/<int:puppy_id>/profile/")
 def puppyProfile(shelter_id, puppy_id):
 	puppy = session.query(Puppy).filter_by(id = puppy_id).one()
 	shelter = session.query(Shelter).filter_by(id = shelter_id).one()
 	return render_template('puppyprofile.html', puppy = puppy, shelter = shelter)
 
+#routing for adding new puppy
 @app.route("/shelters/<int:shelter_id>/puppies/new/", methods=['GET','POST'])
 def addNewPuppy(shelter_id):
 	shelter = session.query(Shelter).filter_by(id = shelter_id).one()
@@ -76,6 +83,7 @@ def addNewPuppy(shelter_id):
 		return redirect(url_for('allPuppies',shelter_id = shelter.id))
 	return render_template('addpuppy.html', shelter = shelter)
 
+#routing for deletion of a puppy
 @app.route("/shelters/<int:shelter_id>/puppies/<int:puppy_id>/delete/")
 def deletePuppy(shelter_id, puppy_id):
 	pup = session.query(Puppy).filter_by(id = puppy_id).one()
@@ -83,11 +91,13 @@ def deletePuppy(shelter_id, puppy_id):
 	session.commit()
 	return redirect(url_for("allPuppies",shelter_id = shelter_id))
 
+#routing for adoption of a puppy
 @app.route("/shelters/<int:shelter_id>/puppies/<int:puppy_id>/adopt/")
 def adoptPuppy(shelter_id, puppy_id):
 	return "<ht>Adobt this adorable puppy</h1>"
 
-@app.route("/shelters/<int:shelter_id>/puppies/<int:puppy_id>/edit/")
+#routing for updating a puppy's information
+@app.route("/shelters/<int:shelter_id>/puppies/<int:puppy_id>/update/")
 def editPuppy(shelter_id,puppy_id):
 	return "<h1>Update information about puppy</h1>"
 
